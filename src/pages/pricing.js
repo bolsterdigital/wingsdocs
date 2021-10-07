568043df648f9e055092925d5e942fe929421e7a
jaapstronks <jaapstronks@bureaubolster.nl>
2021-10-07 22:43:38 +0200

    more missing translations

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import Pricings from '../components/Pricing';
 import styles from './pricing.module.css';
+import Translate, { translate } from '@docusaurus/Translate';
 
 function Pricing() {
   return (
     <Layout title="Pricing">
       <div className={styles.pricingcontainer}>
-        <h1>We've got great plans for you.</h1>
+        <h1>
+          <Translate>We've got great plans for you.</Translate>
+        </h1>
         <p>
-          From the smallest grassroots neighbourhood initiative to global,
-          multilingual federated website infrastructure: Wings can accomodate
-          any progressive campaign.
+          <Translate>
+            {' '}
+            From the smallest grassroots neighbourhood initiative to global,
+            multilingual federated website infrastructure: Wings can accomodate
+            any progressive campaign.
+          </Translate>
         </p>
       </div>
 
